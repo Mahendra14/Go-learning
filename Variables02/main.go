9921e0b5b75f2893165ac77b986453d3ddf8a3bd
Mahendra Kumar Raichur <mahendra.raichur@gmail.com>
2021-09-11 21:11:07 +0530

    Learning abt variables

---
+++ Variables02/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("Variables")
+	//String variable
+	var stringVar string = "Mahenda"
+	fmt.Println(stringVar)
+	fmt.Printf("The type of the variable stringVar is : %T \n", stringVar)
+
+}
