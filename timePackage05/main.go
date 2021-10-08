1d185c228ed6a77370c5967b85a97f26416616c4
Mahendra Kumar Raichur <mahendra.raichur@gmail.com>
2021-10-08 18:05:46 +0530

    to format time and its eccentricities

--- timePackage05/main.go
+++ timePackage05/main.go
@@ -11,4 +11,11 @@ func main() {
 	presentTime := time.Now()
 
 	fmt.Println(presentTime)
+
+	//in a particulat format
+	// for he date we have to use the ones given below for sure:
+	// 01-02-2006
+	//Monday
+	//15:04:05
+	fmt.Println(presentTime.Format("01-02-2006 Monday 15:04:05"))
 }
