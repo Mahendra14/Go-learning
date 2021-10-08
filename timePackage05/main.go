01aff07a5492ee08e7de690def99a7d9d1e7de97
Mahendra Kumar Raichur <mahendra.raichur@gmail.com>
2021-10-08 18:08:26 +0530

    created cutsom date and printed and also some notes

--- timePackage05/main.go
+++ timePackage05/main.go
@@ -18,4 +18,10 @@ func main() {
 	//Monday
 	//15:04:05
 	fmt.Println(presentTime.Format("01-02-2006 Monday 15:04:05"))
+
+	//custom date creation
+
+	//we can make use of same format method to format this date too.
+	createdDate := time.Date(2020, time.March, 18, 18, 18, 0, 0, time.UTC)
+	fmt.Println("Printing the created Date : ", createdDate)
 }
